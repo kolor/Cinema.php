0b06aedc8adc172add97380ad10c929b400b89ea
Tom Sendze <kolor9@gmail.com>
2013-01-25 18:49:02 +0100

    cleaned configs

--- application/bundles.php
+++ application/bundles.php
@@ -4,12 +4,6 @@
 |--------------------------------------------------------------------------
 | Bundle Configuration
 |--------------------------------------------------------------------------
-|
-| Bundles allow you to conveniently extend and organize your application.
-| Think of bundles as self-contained applications. They can have routes,
-| controllers, models, views, configuration, etc. You can even create
-| your own bundles to share with the Laravel community.
-|
 | This is a list of the bundles installed for your application and tells
 | Laravel the location of the bundle's root directory, as well as the
 | root URI the bundle responds to.
@@ -22,10 +16,6 @@
 |			'location' => 'admin',
 |			'handles'  => 'admin',
 |		),
-|
-| Note that the "location" is relative to the "bundles" directory.
-| Now the bundle will be recognized by Laravel and will be able
-| to respond to requests beginning with "admin"!
 |
 | Have a bundle that lives in the root of the bundle directory
 | and doesn't respond to any requests? Just add the bundle
--- application/routes.php
+++ application/routes.php
@@ -9,27 +9,6 @@
 | breeze to setup your application using Laravel's RESTful routing and it
 | is perfectly suited for building large applications and simple APIs.
 |
-| Let's respond to a simple GET request to http://example.com/hello:
-|
-|		Route::get('hello', function()
-|		{
-|			return 'Hello World!';
-|		});
-|
-| You can even respond to more than one URI:
-|
-|		Route::post(array('hello', 'world'), function()
-|		{
-|			return 'Hello World!';
-|		});
-|
-| It's easy to allow URI wildcards using (:num) or (:any):
-|
-|		Route::put('hello/(:any)', function($name)
-|		{
-|			return "Welcome, $name.";
-|		});
-|
 */
 
 Route::get('/', function()
@@ -45,10 +24,6 @@ Route::get('/', function()
 | To centralize and simplify 404 handling, Laravel uses an awesome event
 | system to retrieve the response. Feel free to modify this function to
 | your tastes and the needs of your application.
-|
-| Similarly, we use an event to handle the display of 500 level errors
-| within the application. These errors are fired when there is an
-| uncaught exception thrown in the application.
 |
 */
 
